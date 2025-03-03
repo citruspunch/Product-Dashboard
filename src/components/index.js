60fa60b4ac0eadb1c2bc66e2ecc9845cf97d9a34
andrestobar05 <andrewtobar05@gmail.com>
2025-03-03 17:36:47 -0600

    add routing and component exports for Home, Product, and Cart pages

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+// This file is used to export all the components in the components folder so that they can be imported in the App.jsx file.
+import Navigation from './Navigation';
+import ProductList from './ProductList';
+import ShoppingCart from './ShoppingCart';
+
+export { Navigation, ProductList, ShoppingCart };
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import CartPage from '/CartPage';
+import HomePage from '/HomePage';
+import ProductPage from '/ProductPage';
+
+export { CartPage, HomePage, ProductPage };
