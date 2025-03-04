971815aa7d163810e37a6ba53628f01f2078842d
andrestobar05 <andrewtobar05@gmail.com>
2025-03-03 22:58:26 -0600

    refactor HomePage to pass products and onAddToCart props to ProductList

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ProductList from '../components/ProductList';
 
-export default function HomePage () {
+export default function HomePage ({ onAddToCart, products }) {
     return (
         <div>
             <h1>Welcome to the Product Dashboard</h1>
             <h2>Featured Products</h2>
-            <ProductList />
+            <ProductList products={products} onAddToCart={onAddToCart} />
         </div>
     );
 };
