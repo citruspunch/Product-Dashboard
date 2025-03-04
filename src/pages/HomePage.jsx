e588228d253440f0be21d6b5df7f49503f382e6d
andrestobar05 <andrewtobar05@gmail.com>
2025-03-04 11:07:02 -0600

    refactor HomePage component to use Chakra UI for layout and styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Box, Heading, Text } from '@chakra-ui/react';
 import ProductList from '../components/ProductList';
 
-export default function HomePage ({ onAddToCart, products }) {
+export default function HomePage({ onAddToCart, products }) {
     return (
-        <div>
-            <h1>Welcome to the Product Dashboard</h1>
-            <h2>Featured Products</h2>
+        <Box p={7}>
+            <Heading as="h1" size="5xl" mb={4} fontWeight="bold" fontFamily="Poppins" textAlign="center">Welcome to the Product Dashboard</Heading>
+            <Text px={5} pt={5} fontWeight="semibold" fontSize="2xl" fontFamily="Poppins" mb={4}>Featured Products</Text>
             <ProductList products={products} onAddToCart={onAddToCart} />
-        </div>
+        </Box>
     );
-};
+}
