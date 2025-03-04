a37d904c60e6ac316fb69f7ddda0670b55395886
andrestobar05 <andrewtobar05@gmail.com>
2025-03-03 22:58:15 -0600

    refactor CartPage to pass cart-related props to ShoppingCart component

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ShoppingCart from '../components/ShoppingCart';
 
-export default function CartPage(){
+export default function CartPage({ cartItems, removeItem, updateQuantity, getTotal }) {
     return (
         <div>
             <h1>Your Shopping Cart</h1>
-            <ShoppingCart />
+            <ShoppingCart cartItems={cartItems} removeItem={removeItem} updateQuantity={updateQuantity} getTotal={getTotal} />
             <button>Checkout</button>
         </div>
     );
