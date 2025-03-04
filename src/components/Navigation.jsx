1e2746d26ee01f779b26a49aa32a82a611afb5be
andrestobar05 <andrewtobar05@gmail.com>
2025-03-04 11:06:17 -0600

    add ColorModeButton to Navigation component and update styling

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { Box, Flex, Link } from '@chakra-ui/react';
+import { ColorModeButton } from './ui/color-mode';
+
 
 export default function Navigation() {
     return (
-        <Box bg="teal.500" p={4}>
+        <Box bg="teal.500" p={5}>
             <Flex as="nav" justify="space-between" align="center">
-                <Link as={RouterLink} to="/" color="white" fontWeight="bold" _hover={{ textDecoration: 'underline' }}>
+                <Link as={RouterLink} to="/" fontWeight="bold" fontFamily="Poppins" _hover={{ textDecoration: 'underline' }}>
                     Home
                 </Link>
-                <Link as={RouterLink} to="/cart" color="white" fontWeight="bold" _hover={{ textDecoration: 'underline' }}>
+                <Link as={RouterLink} to="/cart" fontWeight="bold" fontFamily="Poppins" _hover={{ textDecoration: 'underline' }}>
                     Shopping Cart
                 </Link>
+                <ColorModeButton />
             </Flex>
         </Box>
     );
