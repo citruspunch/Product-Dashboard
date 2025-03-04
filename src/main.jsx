519b838ea58b1fb725a3a1a7ad889f672a5035c5
andrestobar05 <andrewtobar05@gmail.com>
2025-03-04 11:06:00 -0600

    wrap App component with ColorModeProvider for theme management

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,15 @@ import { Provider } from "./components/ui/provider"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { ColorModeProvider } from './components/ui/color-mode';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider>
-      <App />
+      <ColorModeProvider>      
+        <App />
+      </ColorModeProvider>
     </Provider>
   </StrictMode>,
 )
