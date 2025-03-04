ddaaf25e11731ed591f1d317f36228caa4b728c1
andrestobar05 <andrewtobar05@gmail.com>
2025-03-04 11:06:51 -0600

    refactor CartPage component to use Chakra UI for layout and styling

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
+import { Box } from '@chakra-ui/react';
 import ShoppingCart from '../components/ShoppingCart';
 
 export default function CartPage({ cartItems, removeItem, updateQuantity, getTotal }) {
     return (
-        <div>
-            <h1>Your Shopping Cart</h1>
+        <Box p={4}>
             <ShoppingCart cartItems={cartItems} removeItem={removeItem} updateQuantity={updateQuantity} getTotal={getTotal} />
-            <button>Checkout</button>
-        </div>
+        </Box>
     );
-};
+}
